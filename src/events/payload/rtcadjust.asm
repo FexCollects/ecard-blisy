53208d1bd8bfa6e926af3b84fcaf738ed02723ec
Fex Collects <fex@fexcollects.com>
2025-10-26 19:48:31 +0000

    add rtc adjust card

---
+++ src/events/payload/rtcadjust.asm
@@ -0,0 +1,16 @@
+INCLUDE "../macros.asm"
+INCLUDE "../constants/items.asm"
+INCLUDE "../constants/scriptcommands.asm"
+
+	Mystery_Event
+
+	db CHECKSUM_CRC
+	dd 0 ; checksum placeholder
+	GBAPTR DataStart
+	GBAPTR DataEnd
+
+DataStart:
+	db CLOCK_ADJUSTMENT
+
+DataEnd:
+	EOF
